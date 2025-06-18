8dc96e88d1d418a41927394160eb9cf80c491ac2
asheses <1273222548@qq.com>
2025-06-18 09:11:55 +0800

    Update for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
+  basePath:"/BlackJack",
   reactStrictMode: true,
   images: {
     unoptimized: true, // 禁用图片优化以支持静态导出
